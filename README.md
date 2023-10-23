e093cc28e93fc3e40946e4d34c6a9a1ff9cc0b15
Lathan Britz <jucallme@gmail.com>
2023-10-23 11:59:25 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# XRPL VALIDATOR VOTE DEAMON
+a deamon to cast votes on your validator from the evernode contract... *todo add link*
+
+## HOW TO INSTALL
+install and add the pm2 pservice on the box https://pm2.keymetrics.io
+copy .env-example to .env adjust as needed
+`$ yarn`
+
+## HOW TO EXECUTE
+`$ ./run.sh
---
+++ src/index.js
@@ -0,0 +1,70 @@
+const { XrplClient } = require('xrpl-client')
+
+const dotenv = require('dotenv')
+const debug = require('debug')
+
+const log = debug('dao-vote:main')
+dotenv.config()
+
+class Main {
+    constructor() {
+        let client = new XrplClient()
+        
+        Object.assign(this, {
+            async listen() {
+                const self = this
+                client.send({
+                    'command': 'subscribe',
+                    'streams': ['ledger']
+                })
+
+                client.on('ledger', async (event) => {
+                    const request = {
+                        'id': 'xrpl-local',
+                        'command': 'ledger',
+                        'ledger_hash': event.ledger_hash,
+                        'ledger_index': 'validated',
+                        'transactions': true,
+                        'expand': true,
+                        'owner_funds': true
+                    }
+                    const ledger_result = await client.send(request)
+                    const transactions = ledger_result?.ledger?.transactions
+                    for (let i = 0; i < transactions.length; i++) {
+                        const transaction = transactions[i]
+                        if (transaction.metaData.TransactionResult !== 'tesSUCCESS') { continue }
+                        if (transaction.TransactionType !== 'AccountSet') { continue }
+                        await self.accountSet(transaction)
+                    }
+                })
+            },
+            async accountSet(transaction) {
+                if (transaction.Account !== process.env.ACCOUNT) { return }
+                log(transaction)
+                const memo = Buffer.from(transaction?.Memos[0]?.Memo.MemoData, 'hex').toString('utf8')
+                log('MEMO', memo)
+                try {
+                    const json = JSON.parse(memo)
+                    log('JSON', json)
+                    if (json.value.length ===0) { return }
+                    const vote = (json.position === 0) ? false:true
+                    const hash = json.value
+
+                    const result = await client.send({
+                        id: 'dao-vote-deamon',
+                        command: 'feature',
+                        feature: hash,
+                        vetoed: vote
+                    })
+
+                    log(`Vote cast on validator, AMM:${hash} VALUE:${vote}`, result)
+                } catch (e) {
+                    // not json and not what looking for ignore
+                }
+            }
+        })
+    }
+}
+
+const app = new Main()
+app.listen()
